4db57ec23eec837ef24a1aa9716c18779da80d62
Truman <truman@Trumans-MacBook-Air.local>
2020-09-13 11:44:38 -0700

    first project commit

---
+++ GEM-ART/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GEM-ART
+//
+//  Created by Truman Tang on 9/13/20.
+//  Copyright © 2020 Truman Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
